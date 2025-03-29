d772c6e55ec041735b4629d265324b538b088d5f
danielway2k3 <22280014@student.hcmus.edu.vn>
2025-03-29 17:45:17 +0700

    fix file topo and controller.

--- controller.py
+++ controller.py
@@ -4,207 +4,118 @@ from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet, ethernet, ether_types, arp, ipv4
-from ryu.lib import hub
-import csv
-import time
 
 class SimpleSwitch13(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch13, self).__init__(*args, **kwargs)
-        self.mac_to_port = {}  # MAC address to switch port mapping
-        self.arp_table = {}  # IP to MAC address mapping
-        self.sw_to_ports = {}  # Switch to ports mapping
-        self.topology = {}  # Switch to connected switches mapping
-        self.datapaths = {}
-        
-        # Create CSV file for metrics
-        self.csv_file = open('network_metrics.csv', 'w', newline='')
-        self.csv_writer = csv.writer(self.csv_file)
-        self.csv_writer.writerow(['timestamp', 'dpid', 'port', 'tx_bytes', 'rx_bytes'])
-        
-        # Start monitoring thread
-        self.monitor_thread = hub.spawn(self._monitor)
-        
-        self.logger.info("Controller initialized")
+        self.mac_to_port = {}
+        self.logger.info("Simple Switch 13 initialized")
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-        
-        # Install table-miss flow entry
+
+        # Install table-miss flow entry (lowest priority)
         match = parser.OFPMatch()
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
-                                         ofproto.OFPCML_NO_BUFFER)]
+                                          ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
-        
-        # Store datapath for monitoring
-        self.datapaths[datapath.id] = datapath
-        self.mac_to_port.setdefault(datapath.id, {})
-        self.sw_to_ports.setdefault(datapath.id, {})
-        
         self.logger.info(f"Switch {datapath.id} connected")
 
-    def add_flow(self, datapath, priority, match, actions, buffer_id=None, idle_timeout=0, hard_timeout=0):
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
-                                           actions)]
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
         if buffer_id:
             mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
-                                  priority=priority, match=match,
-                                  instructions=inst, idle_timeout=idle_timeout,
-                                  hard_timeout=hard_timeout)
+                                    priority=priority, match=match,
+                                    instructions=inst)
         else:
             mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                  match=match, instructions=inst,
-                                  idle_timeout=idle_timeout, hard_timeout=hard_timeout)
+                                    match=match, instructions=inst)
         datapath.send_msg(mod)
-
+        
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
-        # If you hit this you might want to increase
-        # the "miss_send_length" of your switch
+        # If you hit this, you might want to increase the
+        # "miss_send_length" of your switch
         if ev.msg.msg_len < ev.msg.total_len:
-            self.logger.debug("Packet truncated: only %s of %s bytes",
-                             ev.msg.msg_len, ev.msg.total_len)
-        
+            self.logger.debug("Packet truncated")
+
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         in_port = msg.match['in_port']
-        
+
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
-        
-        # Ignore LLDP packets
-        if eth.ethertype == ether_types.ETH_TYPE_IP:
-            ip_pkt = pkt.get_protocol(ipv4.ipv4)
-            if ip_pkt:
-                self.logger.info(f"Handling IPv4 packet from {src} to {dst}")
 
-                if dst in self.mac_to_port[dpid]:
-                    out_port = self.mac_to_port[dpid][dst]
-                else:
-                    out_port = ofproto.OFPP_FLOOD  # Không biết, thì flood
-
-                actions = [parser.OFPActionOutput(out_port)]
-                
-                # Cài đặt flow mới để tránh gọi lại packet_in
-                match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src, eth_type=ether_types.ETH_TYPE_IP)
-                self.add_flow(datapath, 1, match, actions, idle_timeout=60)
-                
-                out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                        in_port=in_port, actions=actions, data=msg.data)
-                datapath.send_msg(out)
-
-        
-        # Get source/destination MAC
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # Ignore LLDP
+            return
+            
         dst = eth.dst
         src = eth.src
         dpid = datapath.id
-        
-        # Learn a mac address to avoid FLOOD next time.
+
+        # Learn a MAC address to port mapping for avoidance of FLOOD next time
         self.mac_to_port.setdefault(dpid, {})
         self.mac_to_port[dpid][src] = in_port
         
-        # Handle ARP packets specifically
-        if eth.ethertype == ether_types.ETH_TYPE_ARP:
-            self.handle_arp(datapath, in_port, pkt)
+        # Log important info for debug 
+        self.logger.info(f"PacketIn: dpid={dpid}, src={src}, dst={dst}, in_port={in_port}")
         
-        # If we know where the destination is, forward to that port
+        if eth.ethertype == ether_types.ETH_TYPE_ARP:
+            self.logger.info(f"ARP packet")
+            
+        if eth.ethertype == ether_types.ETH_TYPE_IP:
+            ip = pkt.get_protocol(ipv4.ipv4)
+            self.logger.info(f"IPv4 packet {ip.src} -> {ip.dst}")
+
+        # If destination MAC is known (learned earlier), use that port, else flood
         if dst in self.mac_to_port[dpid]:
             out_port = self.mac_to_port[dpid][dst]
-            self.logger.info(f"Destination {dst} found at port {out_port} of switch {dpid}")
+            self.logger.info(f"Out port found: {out_port}")
         else:
             out_port = ofproto.OFPP_FLOOD
-            self.logger.info(f"Destination {dst} not found, flooding from switch {dpid}")
-        
+            self.logger.info(f"Flooding")
+
         actions = [parser.OFPActionOutput(out_port)]
-        
+
         # Install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
-            # Verify if we have a valid buffer_id, if yes avoid to send both
+            # Check IP protocol
+            if eth.ethertype == ether_types.ETH_TYPE_IP:
+                ip = pkt.get_protocol(ipv4.ipv4)
+                match = parser.OFPMatch(
+                    in_port=in_port,
+                    eth_type=ether_types.ETH_TYPE_IP,
+                    ipv4_src=ip.src,
+                    ipv4_dst=ip.dst
+                )
+            else:
+                # Use simple L2 match for non-IP traffic
+                match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+                
+            # Verify if we have a valid buffer_id, if yes avoid sending both
             # flow_mod & packet_out
             if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                self.add_flow(datapath, 1, match, actions, buffer_id=msg.buffer_id, idle_timeout=60)
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
                 return
             else:
-                self.add_flow(datapath, 1, match, actions, idle_timeout=60)
-        
+                self.add_flow(datapath, 1, match, actions)
+
         # Send packet out
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
-        
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                in_port=in_port, actions=actions, data=data)
-        datapath.send_msg(out)
-        
-        self.logger.info(f"Packet handled: dpid={dpid}, src={src}, dst={dst}, in_port={in_port}, out_port={out_port}")
-    
-    def handle_arp(self, datapath, in_port, pkt):
-        eth = pkt.get_protocols(ethernet.ethernet)[0]
-        arp_pkt = pkt.get_protocol(arp.arp)
-        
-        if not arp_pkt:
-            return
-        
-        # Update ARP table
-        self.arp_table[arp_pkt.src_ip] = eth.src
-        self.logger.info(f"ARP: {arp_pkt.src_ip} -> {eth.src}")
-    
-    def _monitor(self):
-        """
-        Periodically request port statistics from switches
-        """
-        self.logger.info('Starting monitoring thread')
-        while True:
-            for dp in self.datapaths.values():
-                self._request_port_stats(dp)
-            hub.sleep(10)  # Request every 10 seconds
-    
-    def _request_port_stats(self, datapath):
-        """
-        Send port statistics request to switch
-        """
-        self.logger.debug('Sending port stats request to switch %016x', datapath.id)
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-        
-        req = parser.OFPPortStatsRequest(datapath, 0, ofproto.OFPP_ANY)
-        datapath.send_msg(req)
-    
-    @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
-    def _port_stats_reply_handler(self, ev):
-        """
-        Handle port statistics reply from switch
-        """
-        body = ev.msg.body
-        dpid = ev.msg.datapath.id
-        
-        self.logger.debug('PortStats from switch %d:', dpid)
-        for stat in body:
-            # Write stats to CSV
-            self.csv_writer.writerow([
-                time.time(),
-                dpid,
-                stat.port_no,
-                stat.tx_bytes,
-                stat.rx_bytes
-            ])
-            self.csv_file.flush()
 
-    def __del__(self):
-        """
-        Clean up when controller is stopped
-        """
-        if hasattr(self, 'csv_file'):
-            self.csv_file.close()
-            self.logger.info("CSV file closed")
+        out = parser.OFPPacketOut(
+            datapath=datapath, buffer_id=msg.buffer_id,
+            in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
--- topology.py
+++ topology.py
@@ -1,141 +1,61 @@
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import RemoteController
-from mininet.link import TCLink
-from mininet.util import dumpNodeConnections
-from mininet.log import setLogLevel
 from mininet.cli import CLI
-import time
-import os
-import signal
-import subprocess
+from mininet.log import setLogLevel
 
-class LoadBalanceTopo(Topo):
+class SimpleTestTopo(Topo):
     def build(self):
-        # Add switches
+        # Add 2 switches
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
-        s3 = self.addSwitch('s3')
-        s4 = self.addSwitch('s4')
         
-        # Add hosts
+        # Add 4 hosts
         h1 = self.addHost('h1')
         h2 = self.addHost('h2')
         h3 = self.addHost('h3')
         h4 = self.addHost('h4')
-        h5 = self.addHost('h5')
-        h6 = self.addHost('h6')
-        
-        # Add links between switches
-        self.addLink(s1, s2, bw=100, delay='5ms', use_htb=True)
-        self.addLink(s1, s3, bw=100, delay='5ms', use_htb=True)
-        self.addLink(s1, s4, bw=100, delay='5ms', use_htb=True)
-        self.addLink(s2, s3, bw=50, delay='5ms', use_htb=True)  # New link to improve connectivity
-        self.addLink(s3, s4, bw=50, delay='5ms', use_htb=True)
         
         # Connect hosts to switches
-        self.addLink(h1, s2, bw=10, delay='1ms', use_htb=True)
-        self.addLink(h2, s2, bw=10, delay='1ms', use_htb=True)
-        self.addLink(h3, s3, bw=10, delay='1ms', use_htb=True)
-        self.addLink(h4, s4, bw=10, delay='1ms', use_htb=True)
-        self.addLink(h5, s4, bw=10, delay='1ms', use_htb=True)
-        self.addLink(h6, s4, bw=10, delay='1ms', use_htb=True)
-
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+        self.addLink(h3, s2)
+        self.addLink(h4, s2)
+        
+        # Connect switches
+        self.addLink(s1, s2)
 
 def run():
-    
-    # Create topology
-    topo = LoadBalanceTopo()
-    
-    # Create Mininet network with remote controller
-    net = Mininet(topo=topo, controller=lambda name: RemoteController(name, ip='127.0.0.1', port=6653), link=TCLink)
+    topo = SimpleTestTopo()
+    controller = lambda name: RemoteController(name, ip='127.0.0.1')
+    net = Mininet(topo=topo, controller=controller)
     
     try:
         # Start network
         net.start()
-        dumpNodeConnections(net.hosts)
         
-        # Wait for network to initialize
-        print("Waiting for network initialization...")
-        time.sleep(10)  # Increase time to allow controller to process initial packets
+        # Print host connections
+        print("\n*** Hosts connections:")
+        for h in net.hosts:
+            print(f"{h.name} {h.MAC()} {h.IP()}")
         
-        # Test connectivity
-        print("Testing connectivity...")
-        net.pingAll(timeout='1')
+        # Run a ping test
+        print("\n*** Testing ping connectivity")
         
-        # Interactive CLI for debugging
-        print("\nEntering CLI mode for troubleshooting. Type 'exit' to proceed with data collection.")
+        h1, h2 = net.get('h1', 'h2')
+        print(f"Pinging {h2.name} from {h1.name}:")
+        print(h1.cmd(f'ping -c 3 {h2.IP()}'))
+        
+        h1, h3 = net.get('h1', 'h3')
+        print(f"Pinging {h3.name} from {h1.name}:")
+        print(h1.cmd(f'ping -c 3 {h3.IP()}'))
+        
+        # Enter CLI mode
+        print("\n*** Starting CLI")
         CLI(net)
-        
-        # Only continue if the network is working
-        print("Checking if network is ready for data collection...")
-        if net.pingAll(timeout='1') > 50:  # If more than 50% packet loss
-            print("ERROR: Network connectivity is poor. Aborting data collection.")
-            return
-        
-        # Proceed with data collection
-        print("Beginning data collection...")
-        collect_network_data(net)
-        
     finally:
-        # Stop network
-        print("Stopping network...")
+        # Clean up
         net.stop()
-        
-
-
-def collect_network_data(net):
-    """Collect network data and save to CSV"""
-    with open('data_plane.csv', 'w') as f:
-        f.write('timestamp,src,dst,throughput_mbps,latency_ms\n')
-        hosts = net.hosts
-        
-        for src in hosts:
-            for dst in hosts:
-                if src != dst:
-                    try:
-                        print(f"Collecting data between {src.name} and {dst.name}...")
-                        
-                        # Kill any existing iperf servers
-                        dst.cmd('pkill -f "iperf -s"')
-                        
-                        # Start iperf server on destination
-                        dst.cmd(f'iperf -s > /dev/null 2>&1 &')
-                        time.sleep(1)  # Wait for server to start
-                        
-                        # Measure latency
-                        ping_output = src.cmd(f'ping -c 5 {dst.IP()}')
-                        latency = -1
-                        if 'min/avg/max/mdev' in ping_output:
-                            latency_parts = ping_output.split('min/avg/max/mdev = ')[1].split('/')[1]
-                            latency = float(latency_parts)
-                        
-                        # Measure throughput
-                        iperf_output = src.cmd(f'iperf -c {dst.IP()} -t 2')
-                        throughput = -1
-                        if 'Mbits/sec' in iperf_output:
-                            throughput_lines = [line for line in iperf_output.split('\n') if 'Mbits/sec' in line]
-                            if throughput_lines:
-                                last_line = throughput_lines[-1]
-                                parts = last_line.split()
-                                for i in range(len(parts) - 1):
-                                    if parts[i+1] == 'Mbits/sec':
-                                        throughput = float(parts[i])
-                                        break
-                        
-                        # Write data to file
-                        f.write(f'{time.time()},{src.name},{dst.name},{throughput},{latency}\n')
-                        f.flush()
-                        
-                        # Stop iperf server
-                        dst.cmd('pkill -f "iperf -s"')
-                        
-                    except Exception as e:
-                        print(f"Error collecting data between {src.name} and {dst.name}: {e}")
-                        f.write(f'{time.time()},{src.name},{dst.name},-1,-1\n')
-                        f.flush()
-    
-    print("Data collection completed.")
 
 if __name__ == '__main__':
     setLogLevel('info')
